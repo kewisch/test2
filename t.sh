9e9348735300501213df6ddb0da278326e8f6ce0
Philipp Kewisch <mozilla@kewis.ch>
2025-03-08 21:33:59 +0100

    hi

--- t.sh
+++ t.sh
@@ -6,24 +6,28 @@
 PR_NUMBER=3
 GITHUB_REPOSITORY=kewisch/test2
 
-gh api graphql -F repoOwner="${GITHUB_REPOSITORY%/*}" -F repoName="${GITHUB_REPOSITORY#*/}" -F prNumber="$PR_NUMBER" -f query='
-  query($repoOwner: String!, $repoName: String!, $prNumber: Int!) {
-    repository(owner: $repoOwner, name: $repoName) {
-      pullRequest(number: $prNumber) {
-        closingIssuesReferences(first: 100) {
-          nodes {
-            number
-          }
-        }
-      }
-    }
-  }' --jq '.data.repository.pullRequest.closingIssuesReferences.nodes.[].number'
+#gh api graphql -F repoOwner="${GITHUB_REPOSITORY%/*}" -F repoName="${GITHUB_REPOSITORY#*/}" -F prNumber="$PR_NUMBER" -f query='
+#  query($repoOwner: String!, $repoName: String!, $prNumber: Int!) {
+#    repository(owner: $repoOwner, name: $repoName) {
+#      pullRequest(number: $prNumber) {
+#        closingIssuesReferences(first: 100) {
+#          nodes {
+#            number
+#          }
+#        }
+#      }
+#    }
+#  }' --jq '.data.repository.pullRequest.closingIssuesReferences.nodes.[].number'
 
 
-ACTIVE_MILESTONE=$(gh api repos/$GITHUB_REPOSITORY/milestones --jq '
+gh api repos/$GITHUB_REPOSITORY/milestones --jq '
       map(select(.state == "open" and .due_on != null))
       | sort_by(.due_on) | reverse
-      | .[0].number' 2>/dev/null || echo "")
+      | "milestone=" + (.[0].number|tostring)'
+#ACTIVE_MILESTONE=$(gh api repos/$GITHUB_REPOSITORY/milestones --jq '
+#      map(select(.state == "open" and .due_on != null))
+#      | sort_by(.due_on) | reverse
+#      | .[0].number' 2>/dev/null || echo "")
 
-echo $ACTIVE_MILESTONE
+#gh api --method PATCH /repos/$GITHUB_REPOSITORY/issues/$PR_NUMBER -f milestone=$ACTIVE_MILESTONE >/dev/null
 # hi
